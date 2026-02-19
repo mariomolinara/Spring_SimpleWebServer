b9fee526f190981838ee1e196835422873352e3b
Mario Molinara <m.molinara@unicas.it>
2026-02-19 08:21:17 +0100

    Readme added

--- src/main/java/it/unicas/spring/webserver/WebServerApplication.java
+++ src/main/java/it/unicas/spring/webserver/WebServerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WebServerApplication.class, args);
+    }
 
 }
